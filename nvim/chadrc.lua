064e6e53c5508d0e75801b15950a18100204a7d6
releng-ci-user <github@elsesiy.com>
2023-10-08 19:41:45 -0700

    Add Ln, Col to statusline

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -1,18 +1,24 @@
 ---@type ChadrcConfig
 local M = {}
 M.ui = {
+	statusline = {
+		separator_style = "block",
+		overriden_modules = function(modules)
+			modules[11] = " Ln %l, Col %c"
+		end,
+	},
 	theme = "catppuccin",
 	transparency = true,
 }
 M.plugins = "custom.plugins"
 M.mappings = require("custom.mappings")
 M.lazy_nvim = {
-  performance = {
-    rtp = {
-      disabled_plugins = vim.tbl_filter(function(name)
-        return string.sub(name, 1, 5) ~= "netrw"
-      end, require("plugins.configs.lazy_nvim").performance.rtp.disabled_plugins),
-    },
-  },
+	performance = {
+		rtp = {
+			disabled_plugins = vim.tbl_filter(function(name)
+				return string.sub(name, 1, 5) ~= "netrw"
+			end, require("plugins.configs.lazy_nvim").performance.rtp.disabled_plugins),
+		},
+	},
 }
 return M
