7793b13554183f2b54e42eaa13f6a83fb4324917
Jonas-Taha El Sesiy <github@elsesiy.com>
2024-06-02 18:39:57 -0700

    Update trouble keymaps for v3

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -11,6 +11,7 @@ local plugins = {
 	},
 	{
 		"nvim-telescope/telescope.nvim",
+		cmd = "Telescope",
 		keys = {
 			{
 				"<leader>tr",
@@ -218,35 +219,40 @@ local plugins = {
 	-- Better diagnostics
 	{
 		"folke/trouble.nvim",
+		cmd = "Trouble",
 		keys = {
 			{
-				"<leader>tx",
-				function()
-					require("trouble").toggle()
-				end,
-				desc = "trouble toggle",
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
 			},
 			{
-				"<leader>tn",
-				function()
-					require("trouble").next({
-						skip_groups = true,
-						jump = true,
-					})
-				end,
-				desc = "trouble next",
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
 			},
 			{
-				"<leader>tp",
-				function()
-					require("trouble").previous({
-						skip_groups = true,
-						jump = true,
-					})
-				end,
-				desc = "trouble previous",
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
 			},
 		},
+		opts = {},
 	},
 
 	{
