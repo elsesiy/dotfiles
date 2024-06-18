406cdeb0b32bc305df9bf12dac0b952fab421dff
Jonas-Taha El Sesiy <github@elsesiy.com>
2024-06-17 19:35:47 -0700

    Update wezterm config

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -4,15 +4,17 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
 config.default_prog = { "zellij" }
+config.disable_default_key_bindings = true
 config.enable_tab_bar = false
 config.font = wezterm.font_with_fallback({
-	{ family = "JetBrains Mono", scale = 1.5 },
-	{ family = "FiraCode", scale = 1.5 },
+	{ family = "JetBrains Mono" },
+	{ family = "FiraCode" },
 })
-config.font_size = 13
-
-config.disable_default_key_bindings = true
-config.disable_default_mouse_bindings = true
+config.font_size = 18
+config.front_end = "WebGpu"
+config.scrollback_lines = 100000
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE"
 
 local act = wezterm.action
 config.keys = {
@@ -21,6 +23,7 @@ config.keys = {
 	{ key = ".", mods = "SUPER", action = act.ActivateCommandPalette },
 	{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
 	{ key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
+	{ key = "h", mods = "SUPER", action = act.HideApplication },
 	{ key = "Enter", mods = "SUPER", action = act.ToggleFullScreen },
 	{ key = ":", mods = "SUPER", action = act.ShowDebugOverlay },
 	{ key = "0", mods = "SUPER", action = act.ResetFontSize },
