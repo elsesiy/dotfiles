e3d6f4b559e56cb0ac301208b036514bac9747c9
Jonas-Taha El Sesiy <github@elsesiy.com>
2024-08-01 00:49:00 -0700

    add more locations to PATH

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -1,13 +1,16 @@
 fish_config theme choose "Catppuccin Mocha"
 
 # path
+fish_add_path ~/.cargo/bin
 fish_add_path ~/.krew/bin
+fish_add_path (go env GOPATH)/bin
 eval (brew shellenv)
 
 # global opts
 set -U fish_greeting
 set -U fish_key_bindings fish_vi_key_bindings
 set -Ux EDITOR nvim
+set -Ux GOPROXY direct
 set -Ux LANG en_US.UTF-8
 set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -Ux RIPGREP_CONFIG_PATH ~/.config/ripgreprc
