bfc26ab502712be45047cdb9c9f7c900f0da68ba
Jonas-Taha El Sesiy <github@elsesiy.com>
2024-08-07 13:57:40 -0700

    add nix fish conf

---
+++ .config/fish/conf.d/nix.fish
@@ -0,0 +1,6 @@
+if test -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+    source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+end
+if test -f /nix/var/nix/profiles/default/share/fish/vendor_completions.d/nix.fish
+    source /nix/var/nix/profiles/default/share/fish/vendor_completions.d/nix.fish
+end
