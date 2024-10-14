f96592d351f017800d9a6fb356dc2fac340beac5
Jonas-Taha El Sesiy <github@elsesiy.com>
2024-10-13 21:46:39 -0700

    fix bat manpager formatting

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -12,4 +12,5 @@ set -U fish_key_bindings fish_vi_key_bindings
 set -Ux EDITOR nvim
 set -Ux LANG en_US.UTF-8
 set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -Ux MANROFFOPT "-c"
 set -Ux RIPGREP_CONFIG_PATH ~/.config/ripgreprc
