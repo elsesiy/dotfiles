72090ac1c958f3a15603c5f78c6c5f60fd9263ad
Jonas-Taha El Sesiy <github@elsesiy.com>
2025-04-07 20:35:56 -0700

    add worktrees.nvim

---
+++ .config/nvim/lua/plugins/worktrees.lua
@@ -0,0 +1,27 @@
+return {
+	"afonsofrancof/worktrees.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- Specify where to create worktrees relative to git common dir
+		-- The common dir is the .git dir in a normal repo or the root dir of a bare repo
+		base_path = "..", -- Parent directory of common dir
+
+		-- Template for worktree folder names
+		-- This is only used if you don't specify the folder name when creating the worktree
+		path_template = "{branch}", -- Default: use branch name
+
+		-- Command names (optional)
+		commands = {
+			create = "WorktreeCreate",
+			delete = "WorktreeDelete",
+			switch = "WorktreeSwitch",
+		},
+
+		-- Key mappings (optional)
+		mappings = {
+			create = "<leader>wtc",
+			delete = "<leader>wtd",
+			switch = "<leader>wts",
+		},
+	},
+}
