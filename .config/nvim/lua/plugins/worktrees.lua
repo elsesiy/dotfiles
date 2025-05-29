0cff0b846b60c64e7bc7c65831ebddda6b38850e
Jonas-Taha El Sesiy <github@elsesiy.com>
2025-05-29 14:22:21 -0700

    update worktrees base path

--- .config/nvim/lua/plugins/worktrees.lua
+++ .config/nvim/lua/plugins/worktrees.lua
@@ -4,7 +4,7 @@ return {
 	opts = {
 		-- Specify where to create worktrees relative to git common dir
 		-- The common dir is the .git dir in a normal repo or the root dir of a bare repo
-		base_path = "..", -- Parent directory of common dir
+		base_path = ".", -- Parent directory of common dir
 
 		-- Template for worktree folder names
 		-- This is only used if you don't specify the folder name when creating the worktree
