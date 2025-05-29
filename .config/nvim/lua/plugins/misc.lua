a7c9b9de15c43455e0f0511f14ffc14e1b5026f3
Jonas-Taha El Sesiy <github@elsesiy.com>
2025-05-29 12:05:53 -0700

    update gitsigns config

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -75,15 +75,74 @@ local plugins = {
 	},
 	{
 		"lewis6991/gitsigns.nvim",
-		keys = {
-			{ "<leader>hP", ":Gitsigns preview_hunk_inline<CR>", { desc = "Preview hunk inline" } },
-			{ "<leader>hR", ":Gitsigns reset_buffer<CR>", { desc = "Reset buffer" } },
-			{ "<leader>hp", ":Gitsigns preview_hunk<CR>", { desc = "Preview hunk" } },
-			{ "<leader>hr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk" } },
-			{ "<leader>hs", ":Gitsigns stage_hunk<CR>", { desc = "Stage hunk" } },
-			{ "<leader>hu", ":Gitsigns undo_stage_hunk<CR>", { desc = "Unstage hunk" } },
-			{ "[h", ":Gitsigns prev_hunk<CR>", { desc = "Previous git hunk" } },
-			{ "]h", ":Gitsigns next_hunk<CR>", { desc = "Next git hunk" } },
+		opts = {
+			numhl = true,
+			current_line_blame = true,
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+					end
+				end)
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end)
+
+				-- Actions
+				map("n", "<leader>hs", gitsigns.stage_hunk)
+				map("n", "<leader>hr", gitsigns.reset_hunk)
+
+				map("v", "<leader>hs", function()
+					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+
+				map("v", "<leader>hr", function()
+					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+
+				map("n", "<leader>hS", gitsigns.stage_buffer)
+				map("n", "<leader>hR", gitsigns.reset_buffer)
+				map("n", "<leader>hp", gitsigns.preview_hunk)
+				map("n", "<leader>hi", gitsigns.preview_hunk_inline)
+
+				map("n", "<leader>hb", function()
+					gitsigns.blame_line({ full = true })
+				end)
+
+				map("n", "<leader>hd", gitsigns.diffthis)
+
+				map("n", "<leader>hD", function()
+					gitsigns.diffthis("~")
+				end)
+
+				map("n", "<leader>hQ", function()
+					gitsigns.setqflist("all")
+				end)
+				map("n", "<leader>hq", gitsigns.setqflist)
+
+				-- Toggles
+				map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+				map("n", "<leader>tw", gitsigns.toggle_word_diff)
+
+				-- Text object
+				map({ "o", "x" }, "ih", gitsigns.select_hunk)
+			end,
 		},
 	},
 
@@ -200,7 +259,7 @@ local plugins = {
 	---- Detect tabstop and shiftwidth automatically, maybe replace with faster alternatives
 	{ "tpope/vim-sleuth", opts = {} },
 	-- { "Darazaki/indent-o-matic", opts = {} },
-  -- { "NMAC427/guess-indent.nvim", opts = {} },
+	-- { "NMAC427/guess-indent.nvim", opts = {} },
 
 	-- Better folds
 	{
