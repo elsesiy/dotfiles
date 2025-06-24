6b76e2b07003223eb1a74b1b6208b9238e0ed6dd
Jonas-Taha El Sesiy <github@elsesiy.com>
2025-06-23 23:12:36 -0700

    add opencode

--- nix-modules/nix-darwin/homebrew/common.nix
+++ nix-modules/nix-darwin/homebrew/common.nix
@@ -2,11 +2,15 @@
   homebrew = {
     brews = [
       "node@24"
+      "sst/tap/opencode"
     ];
     casks = [
       "ferdium"
       "ghostty" # nixpks broken
       "uhk-agent" # nixpks doesn't support arm
+    ];
+    taps = [
+      "sst/homebrew-tap"
     ];
     enable = true;
     # updates homebrew packages on activation,
