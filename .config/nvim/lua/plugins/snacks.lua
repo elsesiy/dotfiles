f0d40d77b3d0c98075f7d3bf062d5287bef8b1ee
Jonas-Taha El Sesiy <github@elsesiy.com>
2025-11-02 17:48:05 -0800

    snack gh integration, remove gh-dash & gh-lite

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -55,6 +55,7 @@ return {
       { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
       { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
       { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+      { "<leader>gh", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
 
       -- grep
       { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
